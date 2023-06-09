1e3b9e02e94736b0b361afc8ef840302a28c7cd4
Raman Samusevich <raman.samusevich@honeywell.com>
2023-06-09 08:22:34 +0200

    adding setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='profun',
+    description='Library for protein function prediction',
+    license='MIT',
+    version='0.1',
+    zip_safe=True,
+    include_package_data=True,
+    packages=find_packages(),
+    )})
