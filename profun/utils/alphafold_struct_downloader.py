6aefd1f19ed5cc0531d5170f630d53c2354f4efa
Raman <raman.samusevich@gmail.com>
2024-08-18 12:22:29 +0200

    modifying the downloader partial function

--- profun/utils/alphafold_struct_downloader.py
+++ profun/utils/alphafold_struct_downloader.py
@@ -35,16 +35,16 @@ def parse_args() -> argparse.Namespace:
     args = parser.parse_args()
     return args
 
-def download_af_struct(uniprot_id, fails_count=0, max_fails_count=3):
-    # try:
-    URL = f"https://alphafold.ebi.ac.uk/files/AF-{uniprot_id}-F1-model_v3.pdb"
-    response = requests.get(URL)
-    with open(root_af / f"{uniprot_id}.pdb", "wb") as file:
-        file.write(response.content)
-    # except:
-    #     logger.warning(f"Error downloading AlphaFold2 structure for {uniprot_id}")
-    #     if fails_count < max_fails_count:
-    #         download_af_struct(uniprot_id, fails_count+1)
+def download_af_struct(uniprot_id, root_af, fails_count=0, max_fails_count=3):
+    try:
+        URL = f"https://alphafold.ebi.ac.uk/files/AF-{uniprot_id}-F1-model_v3.pdb"
+        response = requests.get(URL)
+        with open(root_af / f"{uniprot_id}.pdb", "wb") as file:
+            file.write(response.content)
+    except:
+        logger.warning(f"Error downloading AlphaFold2 structure for {uniprot_id}")
+        if fails_count < max_fails_count:
+            download_af_struct(uniprot_id, fails_count+1)
 
 
 def main():
@@ -56,11 +56,13 @@ def main():
     if not root_af.exists():
         root_af.mkdir()
 
+    download_af_struct_for_current_root = lambda uniprot_id: download_af_struct(uniprot_id, root_af)
+
     with open(cl_args.path_to_file_with_ids, 'r') as file:
         all_ids_of_interest = [line.strip() for line in file.readlines()]
 
     with Pool(processes=cl_args.n_jobs) as pool:
-        pool.map(download_af_struct, all_ids_of_interest)
+        pool.map(download_af_struct_for_current_root, all_ids_of_interest)
 
 
 if __name__ == "__main__":
